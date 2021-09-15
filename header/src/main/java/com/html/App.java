991717a96727a8dbd082bfd07f1847d06abce2b6
leo200412 <leomontanogarcia.eljefe@gmail.com>
2021-09-15 17:21:25 -0500

    header proceso2

---
+++ header/src/main/java/com/html/App.java
@@ -0,0 +1,13 @@
+package com.html;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
